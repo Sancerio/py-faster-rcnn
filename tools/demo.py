99e2021f73b163f693276ddd97c5e70c56710782
L <Sancerio>
2015-10-24 11:57:08 +0800

    use webcam as input of faster-rcnn

--- tools/demo.py
+++ tools/demo.py
@@ -17,6 +17,7 @@ import _init_paths
 from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
 from fast_rcnn.nms_wrapper import nms
+from rpn.generate import im_proposals
 from utils.timer import Timer
 import matplotlib.pyplot as plt
 import numpy as np
@@ -41,61 +42,68 @@ def vis_detections(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
+        print "no " + class_name+ " detected."
         return
-
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
+    boxes_scores = []
+    #scores = []
+    # im = im[:, :, (2, 1, 0)]
+    # fig, ax = plt.subplots(figsize=(12, 12))
+    # ax.imshow(im, aspect='equal')
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
+        boxes_scores.append((bbox,score))
+        #scores.append(score)
+        # ax.add_patch(
+        #     plt.Rectangle((bbox[0], bbox[1]),
+        #                   bbox[2] - bbox[0],
+        #                   bbox[3] - bbox[1], fill=False,
+        #                   edgecolor='red', linewidth=3.5)
+        #     )
 
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-            )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
-
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                  fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
+        # ax.text(bbox[0], bbox[1] - 2,
+        #         '{:s} {:.3f}'.format(class_name, score),
+        #         bbox=dict(facecolor='blue', alpha=0.5),
+        #         fontsize=14, color='white')
+    return boxes_scores
 
 def demo(net, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
 
     # Load the demo image
-    im_file = os.path.join(cfg.ROOT_DIR, 'data', 'demo', image_name)
+    im_file = os.path.join(cfg.ROOT_DIR, image_name + '.jpg')
     im = cv2.imread(im_file)
 
     # Detect all object classes and regress object bounds
     timer = Timer()
     timer.tic()
-    scores, boxes = im_detect(net, im)
-    timer.toc()
-    print ('Detection took {:.3f}s for '
+    if im != None:
+        #rbox, rscore = im_proposals(net, im)
+        scores, boxes = im_detect(net, im)#, rbox)
+        timer.toc()
+        print ('Detection took {:.3f}s for '
            '{:d} object proposals').format(timer.total_time, boxes.shape[0])
 
-    # Visualize detections for each class
-    CONF_THRESH = 0.8
-    NMS_THRESH = 0.3
-    for cls_ind, cls in enumerate(CLASSES[1:]):
-        cls_ind += 1 # because we skipped background
-        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
-        cls_scores = scores[:, cls_ind]
-        dets = np.hstack((cls_boxes,
+        # Visualize detections for each class
+        CONF_THRESH = 0.8
+        NMS_THRESH = 0.3
+        rslt = []
+        for cls_ind, cls in enumerate(CLASSES[1:]):
+            cls_ind += 1 # because we skipped background
+            cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+            cls_scores = scores[:, cls_ind]
+            dets = np.hstack((cls_boxes,
                           cls_scores[:, np.newaxis])).astype(np.float32)
-        keep = nms(dets, NMS_THRESH)
-        dets = dets[keep, :]
-        vis_detections(im, cls, dets, thresh=CONF_THRESH)
+            keep = nms(dets, NMS_THRESH)
+            dets = dets[keep, :]
+            boxes_scores = vis_detections(im, cls, dets, thresh=CONF_THRESH)
+            if boxes_scores is not None:
+                rslt.append((boxes_scores, cls))
+
+
+    #if not rslt:
+        print rslt
+        return rslt
 
 def parse_args():
     """Parse input arguments."""
@@ -106,7 +114,7 @@ def parse_args():
                         help='Use CPU mode (overrides --gpu)',
                         action='store_true')
     parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
-                        choices=NETS.keys(), default='vgg16')
+                        choices=NETS.keys(), default='zf')
 
     args = parser.parse_args()
 
@@ -140,11 +148,53 @@ if __name__ == '__main__':
     for i in xrange(2):
         _, _= im_detect(net, im)
 
-    im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-                '001763.jpg', '004545.jpg']
-    for im_name in im_names:
-        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-        print 'Demo for data/demo/{}'.format(im_name)
-        demo(net, im_name)
+    import time
+    # Start here
+    cap = cv2.VideoCapture(0)
+    detected = []
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    current = time.time()
+    previous = current
+    #init = True
+    while (True):
+        ret, frame = cap.read()
+        
 
-    plt.show()
+        key = cv2.waitKey(1) & 0xFF
+        current = time.time() 
+
+        if key == ord('q'):
+            break
+        if  (current - previous) > 1:
+            cv2.imwrite(cfg.ROOT_DIR + '/' + 'frame' + '.jpg', frame)
+            image_path = [cfg.ROOT_DIR + '/' + 'frame' + '.jpg']
+            #print image_path
+            previous = current
+            #if init:
+                #init = False
+        
+            #get_windows(image_path)
+            #print("Processed {} images in {:.3f} s".format(
+                #len(image_path), time.time() - t))
+            
+            detected = demo(net, 'frame')
+
+            #plt.show()
+
+        for x in detected:
+            #print x[1],x[0][0][0]
+            #,x[1]
+            for y in x[0]:
+                #print y[0][0]
+                #print y[1]
+                cv2.rectangle(frame, (y[0][0], y[0][1]), 
+                    (y[0][2], y[0][3]), (0,255,0), 3)        
+                cv2.putText(frame, x[1],# + str(y[1]), 
+                    ( int ((y[0][0] + y[0][2])/2), 
+                      int ((y[0][1] + y[0][3])/2) ), 
+                    font, 1 , (255,0,0), 2, cv2.CV_AA)
+
+        cv2.imshow('frame', frame)
+
+    cap.release()
+    cv2.destroyAllWindows()
